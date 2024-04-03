4a5d81e409ed23fbcab4b34b80fe1b90e4d628d7
chandramohan <support@fritado.com>
2024-04-03 18:38:00 +0530

    second

--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -3,42 +3,12 @@ import BlogGroup from "../components/BlogComponents/BlogGroup";
 import Breadcrumb from "../components/commonComp/Breadcrumb";
 import Footer from "../components/commonComp/Footer";
 import Header from "../components/commonComp/Header";
-import { getAllBlogPosts } from "../services/operations/PostAPI";
+
 import axios from "axios";
 const BASE_URL = process.env.REACT_APP_BASE_URL
 
 const Blog = () => {
-  // const [posts, setPosts] = useState([]);
-  // const [noResults, setNoResults] = useState(false);
-
-  // const fetchPosts = async() =>{
-  //   try{
-  //     // const res = await axios.get(BASE_URL + "/post/getAllPost");
-  //     // console.log(res);
-
-  //   }
-  //   catch(error){
-  //     console.log(error)
-  //   }
-  // }
-
-  // useEffect(() => {
-  //   const getPostDetails = async () => {
-  //     try {
-  //       const res = await getAllBlogPosts();
-  //       console.log("Printing blog details", res);
-  //       setPosts(res);
-
-  //       if (res.data.length === 0) {
-  //         setNoResults(true);
-  //       }
-  //     } catch (error) {
-  //       console.log(error);
-  //     }
-  //   };
-  //   fetchPosts();
-  // },[]);
-
+ 
   
   return (
     <>
